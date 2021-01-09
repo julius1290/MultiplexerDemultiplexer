0d548fa49ee366d6da2f2ff010ccc9f990236d45
Julius Schinschke <mail@julius-schinschke.de>
2021-01-09 22:22:24 +0100

    build rom and cable

--- Storage/BasicRom.py
+++ Storage/BasicRom.py
@@ -5,13 +5,21 @@ class BasicRom:
 
     storage: any
     output_wire: BusWire
+    size: int
 
     def __init__(self, length: int):
+        self.size = length
         self.storage = [None for x in range(length)]
         self.output_wire = BusWire()
 
     def add_instruction(self, pos: int, instruction: any):
+        if pos > self.size or pos < 0:
+            return -1
         self.storage[pos] = instruction
+        return 1
 
     def read_instruction(self, pos: any):
+        if pos > self.size or pos < 0:
+            return -1
         self.output_wire.set_data(self.storage[pos])
+        return 1
--- main.py
+++ main.py
@@ -4,21 +4,24 @@ from Base.BusWire import BusWire
 
 
 ram = BasicRam(8, 8)
+rom = BasicRom(10)
 ram.store(111011001)
 
 wire1 = BusWire()
 wire2 = BusWire()
 wire3 = BusWire()
 
+wire1.set_input(rom.output_wire.output)
 wire2.set_input(wire1.output)
 wire3.set_input(wire2.output)
 wire1.set_data(1)
 print(wire2.get_data())
 
-rom = BasicRom(10)
 rom.add_instruction(0, "111011101")
-wire1.set_input(rom.output_wire.output)
+rom.add_instruction(1,"Hello, World")
 print(rom.storage)
 rom.read_instruction(0)
 print(wire3.get_data())
 print(wire1.get_data())
+rom.read_instruction(1)
+print(wire3.get_data())
